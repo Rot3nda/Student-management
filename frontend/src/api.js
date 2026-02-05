d98c0b068342808ed40af6b6a461c91c12d4c1da
Rotenda Ramugondo <rotendaramugondo@Rotendas-MacBook-Pro.local>
2026-02-05 18:46:23 +0200

    update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://host:5001/api";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5001/api";
 
 export default axios.create({ baseURL });
-
-
-
