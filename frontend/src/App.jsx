0285ed2e1e372e48c9356067b1ada562151d4436
Rot3nda <rotenda.ra@yahoo.com>
2026-02-06 13:57:29 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"; 
 import API from "./api";
 import StudentForm from "./components/StudentForm";
 import StudentList from "./components/StudentList";
